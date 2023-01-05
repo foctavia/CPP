1ca6ac5f8312815146757f3ddc6a8a054eea9c55
foctavia <fo.fanyoctavia@gmail.com>
2023-01-05 10:40:26 +0100

    Edit display Harl programme

--- Module01/ex05/src/main.cpp
+++ Module01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: foctavia <foctavia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 23:09:44 by foctavia          #+#    #+#             */
-/*   Updated: 2023/01/05 10:12:02 by foctavia         ###   ########.fr       */
+/*   Updated: 2023/01/05 10:37:52 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	main( void )
 	Harl		harl;
 	std::string	input = "\0";
 
-	std::cout << "DEBUG" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
 	harl.complain("DEBUG");
-	std::cout << std::endl << "INFO" << std::endl;
+	std::cout << std::endl << "[ INFO ]" << std::endl;
 	harl.complain("INFO");
-	std::cout << std::endl << "WARNING" << std::endl;
+	std::cout << std::endl << "[ WARNING ]" << std::endl;
 	harl.complain("WARNING");
-	std::cout << std::endl << "ERROR" << std::endl;
+	std::cout << std::endl << "[ ERROR ]" << std::endl;
 	harl.complain("ERROR");
 	std::cout << std::endl;
 	
