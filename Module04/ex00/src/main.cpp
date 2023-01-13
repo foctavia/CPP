40ec50bb6dcece3e8858dffef83b71fe3f730104
Fany Octavia <foctavia@student.42.fr>
2023-01-13 19:13:15 +0100

    Add more comment structure for the main

--- Module04/ex00/src/main.cpp
+++ Module04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: foctavia <foctavia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:33:01 by foctavia          #+#    #+#             */
-/*   Updated: 2023/01/10 00:35:39 by foctavia         ###   ########.fr       */
+/*   Updated: 2023/01/13 15:28:32 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ int main( void )
 {
 	std::cout << "********************ANIMAL CLASS********************" << std::endl << std::endl;
 	{	
+		std::cout << "*** Constructing ***" << std::endl << std:: endl;
+		
 		const Animal* meta = new Animal();
 		const Animal* j = new Dog();
 		const Animal* i = new Cat();
 
-		std::cout << std::endl;
+		std::cout << std::endl << "*** Testing ***" << std::endl << std::endl;
 		
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
+		std::cout << "Animal type : " << j->getType() << " " << std::endl;
+		std::cout << "Animal type : " << i->getType() << " " << std::endl;
 		
 		std::cout << std::endl;
 
@@ -35,7 +37,7 @@ int main( void )
 		j->makeSound();
 		meta->makeSound();
 
-		std::cout << std::endl;
+		std::cout << std::endl << "*** Destructing ***" << std::endl << std::endl;
 
 		delete meta;
 		delete j;
@@ -45,22 +47,30 @@ int main( void )
 	}
 	std::cout << "******************WRONGANIMAL CLASS******************" << std::endl << std::endl;
 	{	
+		std::cout << "*** Constructing ***" << std::endl << std:: endl;
+		
 		const WrongAnimal* meta = new WrongAnimal();
 		const WrongAnimal* k = new WrongCat();
+		const WrongCat* l = new WrongCat();
 
-		std::cout << std::endl;
+		std::cout << std::endl << "*** Testing ***" << std::endl << std::endl;
 
-		std::cout << k->getType() << " " << std::endl;
+
+		std::cout << "Animal type : " << k->getType() << " " << std::endl;
+		std::cout << "Animal type : " << l->getType() << " " << std::endl;
+
 		
 		std::cout << std::endl;
 
 		k->makeSound();
+		l->makeSound();
 		meta->makeSound();
 		
-		std::cout << std::endl;
+		std::cout << std::endl << "*** Destructing ***" << std::endl << std::endl;
 
 		delete meta;
 		delete k;
+		delete l;
 
 		std::cout << std::endl;
 	}
